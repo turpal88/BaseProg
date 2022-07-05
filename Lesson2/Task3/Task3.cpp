0093a98f40ff7e03b9b0c1536dd3f5eab2432962
Ivan Buldakov <pereslav88@gmail.com>
2022-07-05 22:29:40 +0300

    Lesson2/Task3 is modified

--- Lesson2/Task3/Task3.cpp
+++ Lesson2/Task3/Task3.cpp
@@ -11,12 +11,12 @@ struct Address {
     int ind;
     
  void print_address() {
-    std::cout << "Страна: " << this->Country << std::endl;
-    std::cout << "Город: " << this->City << std::endl;
-    std::cout << "Улица: " << this->Street << std::endl;
-    std::cout << "Номер дома: " << this->house_num << std::endl;
-    std::cout << "Номер квартиры: " << this->flat_num << std::endl;
-    std::cout << "Индекс: " << this->ind << std::endl;
+    std::cout << "Страна: " << Country << std::endl;
+    std::cout << "Город: " << City << std::endl;
+    std::cout << "Улица: " << Street << std::endl;
+    std::cout << "Номер дома: " << house_num << std::endl;
+    std::cout << "Номер квартиры: " << flat_num << std::endl;
+    std::cout << "Индекс: " << ind << std::endl;
     std::cout << std::endl;
     }
 
