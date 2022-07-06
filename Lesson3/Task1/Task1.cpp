45b6713ee0b12098fd5b5be2bbc0dee5b187ee7d
Ivan Buldakov <pereslav88@gmail.com>
2022-07-06 10:14:12 +0300

    Class Calculator has been modified

--- Lesson3/Task1/Task1.cpp
+++ Lesson3/Task1/Task1.cpp
@@ -1,6 +1,4 @@
-﻿
-
-#include <iostream>
+﻿#include <iostream>
 #include <windows.h>
 class Calculator {
     
@@ -8,6 +6,28 @@ private:
     double num1;
     double num2;
 
+    
+    double add() {
+        return num1 + num2;
+    }
+    double multiply() {
+        return num1 * num2;
+    }
+    double subtract_1_2() {
+        return num1 - num2;
+    }
+    double subtract_2_1() {
+        return num2 - num1;
+    }
+    double divide_1_2() {
+        return num1 / num2;
+    }
+    double divide_2_1() {
+        return num2 / num1;
+    }
+    
+    
+public:
     bool set_num1(double num1) {
         if (num1 == 0) {
             std::cout << "Неверный ввод!" << std::endl;
@@ -28,41 +48,7 @@ private:
             return true;
         }
     }
-    double add() {
-        return num1 + num2;
-    }
-    double multiply() {
-        return num1 * num2;
-    }
-    double subtract_1_2() {
-        return num1 - num2;
-    }
-    double subtract_2_1() {
-        return num2 - num1;
-    }
-    double divide_1_2() {
-        return num1 / num2;
-    }
-    double divide_2_1() {
-        return num2 / num1;
-    }
-    void ask_user_to_input() {
-        double num1 = 0;
-        double num2 = 0;
-        do {
-            
-            std::cout << "Введите num1: ";
-            std::cin >> num1;
-        } while (!set_num1(num1));
-
-        do {
-            
-            std::cout << "Введите num2: ";
-            std::cin >> num2;
-        } while (!set_num2(num2));
-
-        std::cout << std::endl;
-    }
+    
     void make_math_action() {
         std::cout << "num1 + num2 = " << add() << std::endl;
         std::cout << "num1 - num2 = " << subtract_1_2() << std::endl;
@@ -72,24 +58,30 @@ private:
         std::cout << "num2 / num1 = " << divide_2_1() << std::endl;
         std::cout << std::endl;
     }
-public:
-    
-    void make_calc() {
-        while (true) {
-            ask_user_to_input();
-            make_math_action();
-        }
-        
-
-    }
 };
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+    double num1 = 0;
+    double num2 = 0;
     Calculator digits1;
-    digits1.make_calc();
+    while (true) {
+        do {
 
+            std::cout << "Введите num1: ";
+            std::cin >> num1;
+        } while (!digits1.set_num1(num1));
 
+        do {
+
+            std::cout << "Введите num2: ";
+            std::cin >> num2;
+        } while (!digits1.set_num2(num2));
+
+        std::cout << std::endl;
+
+        digits1.make_math_action();
+    }
+    
 }
-
