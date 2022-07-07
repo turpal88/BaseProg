b5d3e8a3d3bbb7ee34bfec91ec93c1f06d6f0545
Ivan Buldakov <pereslav88@gmail.com>
2022-07-07 11:12:04 +0300

    Methods of Calculator have been modified

--- Lesson3/Task1/Task1.cpp
+++ Lesson3/Task1/Task1.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <windows.h>
+
 class Calculator {
     
 private:
@@ -7,24 +8,7 @@ private:
     double num2;
 
     
-    double add() {
-        return num1 + num2;
-    }
-    double multiply() {
-        return num1 * num2;
-    }
-    double subtract_1_2() {
-        return num1 - num2;
-    }
-    double subtract_2_1() {
-        return num2 - num1;
-    }
-    double divide_1_2() {
-        return num1 / num2;
-    }
-    double divide_2_1() {
-        return num2 / num1;
-    }
+    
     
     
 public:
@@ -49,16 +33,28 @@ public:
         }
     }
     
-    void make_math_action() {
-        std::cout << "num1 + num2 = " << add() << std::endl;
-        std::cout << "num1 - num2 = " << subtract_1_2() << std::endl;
-        std::cout << "num2 - num1 = " << subtract_2_1() << std::endl;
-        std::cout << "num1 * num2 = " << multiply() << std::endl;
-        std::cout << "num1 / num2 = " << divide_1_2() << std::endl;
-        std::cout << "num2 / num1 = " << divide_2_1() << std::endl;
-        std::cout << std::endl;
+    double add() {
+        return num1 + num2;
+    }
+    double multiply() {
+        return num1 * num2;
+    }
+    double subtract_1_2() {
+        return num1 - num2;
+    }
+    double subtract_2_1() {
+        return num2 - num1;
+    }
+    double divide_1_2() {
+        return num1 / num2;
+    }
+    double divide_2_1() {
+        return num2 / num1;
     }
 };
+
+    
+
 int main()
 {
     SetConsoleCP(1251);
@@ -81,7 +77,13 @@ int main()
 
         std::cout << std::endl;
 
-        digits1.make_math_action();
+        std::cout << "num1 + num2 = " << digits1.add() << std::endl;
+        std::cout << "num1 - num2 = " << digits1.subtract_1_2() << std::endl;
+        std::cout << "num2 - num1 = " << digits1.subtract_2_1() << std::endl;
+        std::cout << "num1 * num2 = " << digits1.multiply() << std::endl;
+        std::cout << "num1 / num2 = " << digits1.divide_1_2() << std::endl;
+        std::cout << "num2 / num1 = " << digits1.divide_2_1() << std::endl;
+        std::cout << std::endl;
     }
-    
+    return 0;
 }
