3ea01889a618f385b09ad3e7d936e24c559ca3a5
Ivan Buldakov <pereslav88@gmail.com>
2022-07-16 10:02:25 +0300

    Lesson5/Task1

---
+++ Lesson5/Task1/Task1.cpp
@@ -0,0 +1,57 @@
+﻿
+
+#include <iostream>
+#include "windows.h"
+
+class Figure {
+
+private:
+    int field;
+
+    
+public:
+    Figure() {
+        field = 0;
+    }
+    Figure(int side_count) {
+        field = side_count;
+    }
+    int print_side_count() {
+        return field;
+    }
+};
+
+class Triangle : public Figure {
+public:
+    Triangle() :Figure(3) {
+    
+    }
+    
+};
+
+class Rect : public Figure {
+public:
+    Rect() :Figure(4) {
+    
+    }
+    
+};
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::cout << "Количество сторон: " << std::endl;
+    Figure figure;
+
+    Triangle triangle;
+    Rect rectangle;
+
+    std::cout << "Фигура: " << figure.print_side_count() << std::endl;
+    std::cout << "Треугольник: " << triangle.print_side_count() << std::endl;
+    std::cout << "Четырёхугольник: " << rectangle.print_side_count() << std::endl;
+
+    return 0;
+}
+
+
