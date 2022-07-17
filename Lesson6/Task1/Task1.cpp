eea775e3686ee26065bf22fd84cafee8d82f3632
Ivan Buldakov <pereslav88@gmail.com>
2022-07-17 14:32:44 +0300

    Lesson6/Task1

---
+++ Lesson6/Task1/Task1.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include "windows.h"
+#include "make_math_operations.h"
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    float num1 = 0, num2 = 0, result = 0;
+    int operation_index = 0;
+    std::cout << "Введите первое число: ";
+    std::cin >> num1;
+    std::cout << "Введите второе число: ";
+    std::cin >> num2;
+    do {
+        std::cout << "Выберите операцию (1 - сложение, 2 вычитание, 3 - умножение, 4 - деление, 5 - возведение в степень): ";
+        std::cin >> operation_index;
+        if (operation_index <= 0 || operation_index > 5) std::cout << "Введите корректный номер математической операции!" << std::endl;
+        else break;
+    } while (true);
+    if (num2 == 0 && operation_index == 4) {
+        std::cout << "Деление на 0 невозможно!";
+        return -1;
+    }
+    else std::cout << make_math(num1, num2, operation_index);
+    
+    return 0;
+
+}
+
---
+++ Lesson6/Task1/make_math_operations.h
@@ -0,0 +1,2 @@
+#pragma once
+float make_math(float num1, float num2, int operation_index);
