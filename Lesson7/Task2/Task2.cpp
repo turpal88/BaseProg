8d5b0b6588c6e621499ce653c15be82be36d24a4
Ivan Buldakov <pereslav88@gmail.com>
2022-07-20 20:01:54 +0300

    Lesson7/Task2

---
+++ Lesson7/Task2/Task2.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+#include "windows.h"
+#define SUB(a, b) ((a) - (b))
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+
+
+    return 0;
+}
+
