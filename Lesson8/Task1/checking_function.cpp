7b58abbca05c5e7b6f0540757b1d6c8109d6fd89
Ivan Buldakov <pereslav88@gmail.com>
2022-07-28 08:08:40 +0300

    checking_function was modified

--- Lesson8/Task1/checking_function.cpp
+++ Lesson8/Task1/checking_function.cpp
@@ -1,10 +1,13 @@
 ﻿#include "checking_function.h"
 #include "bad_length.h"
 #include <iostream>
-
+#include <string>
 int checking_function(std::string str, int forbidden_length) {
 	
-	if (str.length() == forbidden_length) throw bad_length("Вы ввели слово запретной длины! До свидания");
+	if (str.length() == forbidden_length) {
+		std::string message = "Вы ввели слово длиной " + std::to_string(str.length()) + " запретной длины! Допустимая длина равна " + std::to_string(forbidden_length) + ". До свидания";
+		throw bad_length(message);
+	}
 	else return str.length();
 	
 }
