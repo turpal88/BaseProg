a59361e0ffdc7fbeacccb28ade75334b64bf3c5c
Ivan Buldakov <pereslav88@gmail.com>
2022-07-30 22:10:07 +0300

    Lesson10/Task1

---
+++ Lesson10/Task1/Task1.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include <iostream>
+#include "windows.h"
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::string name = "";
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+	std::cout << "Здравствуйте, " << name << "!" << std::endl;
+	return 0;
+}
+
+
