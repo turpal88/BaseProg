c467f07a2b47bc9ee88a84cf2173c3c61d5d966a
Ivan Buldakov <pereslav88@gmail.com>
2022-08-09 09:29:26 +0300

    Lesson11/Task4

--- Lesson6/Task3/equalsidetriangle.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-#include "triangle.h"
-
-class EqualSideTriangle :public Triangle {
-public:
-
-    EqualSideTriangle(int a, int b, int A, int B);
-};
--- Lesson6/Task3/recttriangle.h
+++
@@ -1,6 +0,0 @@
-﻿#pragma once
-#include "triangle.h"
-class RectTriangle :public Triangle {
-public:
-    RectTriangle(int a, int b, int c, int A, int B);
-};
